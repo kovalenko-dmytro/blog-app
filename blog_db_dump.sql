c5634addc2e2fb917d4ab8f2db76d32e4439c746
kovalenko-dmytro <33664169+kovalenko-dmytro@users.noreply.github.com>
2018-01-11 19:08:55 +0200

    Add files via upload

---
+++ blog_db_dump.sql
@@ -0,0 +1,136 @@
+﻿CREATE DATABASE  IF NOT EXISTS `blog_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `blog_db`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: blog_db
+-- ------------------------------------------------------
+-- Server version	5.7.19-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles` (
+  `article_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `text` longtext NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  PRIMARY KEY (`article_id`),
+  KEY `category_id_idx` (`category_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `position` int(11) NOT NULL,
+  `path` varchar(45) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Java',0,0,''),(2,'PHP',0,1,''),(3,'JavaScript',0,2,''),(4,'Build Tools',1,0,''),(5,'Ant',4,0,'/categories/java/buid-tools/ant'),(6,'Maven',4,1,'/categories/java/buid-tools/maven'),(7,'Gradle',4,2,'/categories/java/buid-tools/gradle'),(8,'JavaSE',1,1,'/categories/java/javase'),(9,'JavaEE',1,2,'/categories/java/javaee'),(11,'PHPCore',2,0,'/categories/php/phpcore'),(12,'PHPFrameworks',2,1,'/categories/php/phpframeworks'),(13,'JSCore',3,0,'/categories/javascript/jscore'),(14,'JSFrameworks',3,1,'/categories/javascript/jsframeworks');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu` (
+  `menu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `position` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `path` varchar(45) NOT NULL,
+  PRIMARY KEY (`menu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` VALUES (1,'Home',0,0,'/'),(2,'About',1,0,''),(3,'Categories',2,0,''),(4,'Contacts',3,0,'/contacts'),(5,'About Blog',0,2,'/about/about-us'),(6,'Our policy',1,2,'/about/policy');
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `alias_name` varchar(45) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `avatar` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (7,'AdaD','Adam','Adams','adams@adams.com','000001','01.jpg'),(8,'BaBr','Bake','Baker','baker@baker.com','000002','02.jpg'),(9,'CaCar','Cart','Carter','carter@carter.com','000003','03.jpg'),(10,'DaDis','Dave','Davis','davis@davis.com','000004','04.jpg'),(11,'Eckhy','Eckhart','Eckhartson','eckhart@eckhart.com','000005','05.jpg'),(12,'Franky','Franklin','Franklins','franklin@franklin.com','000006','06.jpg'),(13,'GraG','Grant','Granton','grant@grant.com','000007','07.jpg'),(14,'HaHl','Hall','Hallowan','hall@hall.com','000008','08.jpg'),(15,'IvIver','Iverson','Iversonaton','iverson@iverson.com','000009','09.jpg'),(16,'JaSon','Jack','Jackson','jack@jack.com','000010','010.jpg'),(17,'KeKe','Kelly','Kelown','kelly@kelly.com','000011','011.jpg'),(18,'LeLy','Lenny','Lennon','Lennon@Lennon.com','000012','012.jpg'),(19,'MaMon','Mady','Madison','madison@madison.com','000013','013.jpg'),(20,'NoNy','Nort','Norton','norton@norton.com','000014','014.jpg'),(21,'PaPo','Paxy','Paxton','paxton@paxton.com','000015','015.jpg');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-11 19:03:44
